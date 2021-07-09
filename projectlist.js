83c725cfa2345d85905116edc1e8e07fed20422f
Tommy Ngoy <tommy.ngoy@outlook.com>
2021-07-08 20:37:32 -0400

    able to add elements to project list

---
+++ projectlist.js
@@ -0,0 +1,39 @@
+const listContainer = document.querySelector("[data-project]")
+const newProject = document.querySelector("[data-new-project]")
+const newListInput = document.querySelector("[data-new-list-input]")
+
+let list = [{
+    id: 1,
+    name: "Project 1"
+}, {
+    id: 2,
+    name: "Project 2"
+}]
+
+newProject.addEventListener("submit", e => {
+    e.preventDefault()
+    const listName = newListInput.value //Words inputted
+    if (listName == null){
+        return
+    }
+    const newList = createList(listName)
+    newListInput.value = null //Empties text field
+    list.push(newList) //Appends newList into list
+    create()
+})
+
+function createList(name){ 
+    return{id: Date.now().toString(), name: name, tasks: []}
+}
+
+function create(){
+    list.forEach(list => {
+        const listElement = document.createElement("li")
+        listElement.classList.add("projects")
+        listElement.dataset.listId = list.id
+        listElement.innerText = list.name
+        listContainer.appendChild(listElement)
+    })
+}
+
+create()
